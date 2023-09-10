a601061577a9ff4adb18fe531bfbd5edc4b18da4
Narasimhananduri <narasimhananduri14@gmail.com>
2023-09-10 23:51:47 +0530

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+    function upt() {
+        var now = new Date();
+
+        document.getElementById("time").innerText =
+            zeroPadding(now.getHours(), 2) + ":" +
+            zeroPadding(now.getMinutes(), 2) + ":" +
+            zeroPadding(now.getSeconds(), 2);
+
+        document.getElementById("date").innerText =
+            now.getFullYear() + "-" +
+            zeroPadding(now.getMonth() + 1, 2) + "-" +
+            zeroPadding(now.getDate(), 2) + "-" +
+            week[now.getDay()];
+    }
+
+    upt(); 
+    setInterval(upt, 1000); 
+
+    function zeroPadding(num, digit) {
+        return String(num).padStart(digit, '0');
+    }
+});
